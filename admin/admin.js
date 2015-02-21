8d9c0168be13182db89f3deabff17cf5e8a6d7e3
Taylor Huston <taylor.a.huston@gmail.com>
2015-02-21 00:50:20 -0700

    Test out creating new HTML from skeleton

--- admin/admin.js
+++ admin/admin.js
@@ -1,1 +1,12 @@
 //JS Admin Functions
+var jsdom = require("jsdom"); 
+var $ = require('jquery')(require("jsdom").jsdom().parentWindow);
+var fs = require('fs');
+
+
+//Function to create new pages
+function createPage() {
+	var newFile = fs.createReadStream('../skeletons/demo.html').pipe(fs.createWriteStream('../index.html'));
+}
+
+createPage();
